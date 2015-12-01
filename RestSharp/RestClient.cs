780162feef0da1e1ee8e34e8a62087e7e4152083
Eamon Woortman <eamon.woortman@gmail.com>
2015-12-01 16:57:01 +0100

    Added a custom certificate validation callback to fix issue #1(SSL Issue with self-signed or SHA265 certificates)

--- RestSharp/RestClient.cs
+++ RestSharp/RestClient.cs
@@ -32,6 +32,10 @@ using System.Net.Cache;
 using System.Security.Cryptography.X509Certificates;
 #endif
 
+#if UNITY
+using System.Net.Security;
+#endif
+
 namespace RestSharp
 {
     /// <summary>
@@ -141,6 +145,8 @@ namespace RestSharp
             this.AddHandler("*", new XmlDeserializer());
 
             this.FollowRedirects = true;
+
+            ServicePointManager.ServerCertificateValidationCallback = SHA256CertificateManualValidation;
         }
 
         /// <summary>
@@ -644,5 +650,27 @@ namespace RestSharp
 
             return response;
         }
+
+#if UNITY
+        private bool SHA256CertificateManualValidation(System.Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) {
+            bool isOk = true;
+            // If there are errors in the certificate chain, look at each error to determine the cause.
+            if (sslPolicyErrors != SslPolicyErrors.None) {
+                for (int i = 0; i < chain.ChainStatus.Length; i++) {
+                    if (chain.ChainStatus[i].Status != X509ChainStatusFlags.RevocationStatusUnknown) {
+                        chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+                        chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+                        chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 1, 0);
+                        chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+                        bool chainIsValid = chain.Build((X509Certificate2)certificate);
+                        if (!chainIsValid) {
+                            isOk = false;
+                        }
+                    }
+                }
+            }
+            return isOk;
+        }
+#endif
     }
 }
